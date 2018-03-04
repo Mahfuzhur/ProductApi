f86904a7ac878627c7da00256a1010cb07d4e4ac
mahfuzh <mahfuzhur.rahman@gmail.com>
2018-03-04 19:22:14 +0600

    Get By Shop Done

---
+++ ProductApi/ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Product")]
+    public class ProductController : Controller
+    {
+        private ProductContext _productContext;
+
+        public ProductController(ProductContext productContext)
+        {
+            _productContext = productContext;
+            if (productContext.ProductItems.Count() == 0)
+            {
+                _productContext.ProductItems.Add(new ProductItem { ShopId = 1, ProductName = "Cola", Price = 18, Quantity = 100 });
+                _productContext.ProductItems.Add(new ProductItem { ShopId = 2, ProductName = "Cola", Price = 16, Quantity = 10 });
+                _productContext.ProductItems.Add(new ProductItem { ShopId = 3, ProductName = "Cola", Price = 20, Quantity = 120 });
+
+                _productContext.SaveChanges();
+
+            }
+        }
+        [HttpGet]
+        public IEnumerable<ProductItem> GetAll()
+        {
+            return _productContext.ProductItems.AsNoTracking().ToList();
+        }
+        [HttpGet("{id}/{shopId}")]
+        public IActionResult GetById(long id, int shopId)
+        {
+            var product = _productContext.ProductItems.FirstOrDefault(p => p.Id==id && p.ShopId == shopId);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(product);
+        }
+    }
+}
---
+++ ProductApi/ProductApi/ProductItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi
+{
+    public class ProductItem
+    {
+        public long Id { get; set; }
+        public string ProductName { get; set; }
+        public int ShopId { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+    }
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductItem> ProductItems { get; set; }
+    }
+
+}
